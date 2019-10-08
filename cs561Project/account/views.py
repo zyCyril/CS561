738e4f212a41f047ac6d5d4edc3ec542083d83f5
Yuxuan Che <chey@oregonstate.edu>
2019-10-07 17:35:33 -0700

    add some web page

--- cs561Project/account/views.py
+++ cs561Project/account/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'account/index.html')
